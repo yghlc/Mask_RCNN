67e48b7a3596b6b92da5e4a7f6ed2b21c551d258
yghlc <hlcgithub123>
2018-07-01 21:06:25 +0800

    files of example

---
+++ run_setting.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+### install pycocotools
+### https://github.com/matterport/Mask_RCNN/issues/6
+### installed pycocotools to site-packages and resolved the import error.
+#
+
+git clone https://github.com/cocodataset/cocoapi.git
+cd PythonAPI
+make
+cp -r pycocotools /home/hlc/programs/anaconda3/lib/python3.6/site-packages/.
+
+
+### use keras 2.0.8 or have issue: https://github.com/matterport/Mask_RCNN/issues/703
